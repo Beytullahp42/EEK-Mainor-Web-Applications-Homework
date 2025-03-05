b09a982daacd67d8b60b2ce814a1010495df05bb
beytp <beytullahpay42@gmail.com>
2025-03-05 22:31:44 +0200

    created the service class...?

--- src/main/java/com/example/springguessingnumbergame/api/GameController.java
+++ src/main/java/com/example/springguessingnumbergame/api/GameController.java
@@ -1,39 +1,26 @@
 package com.example.springguessingnumbergame.api;
 
+import com.example.springguessingnumbergame.services.GuessingNumberGame;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-
 @RestController
+@AllArgsConstructor
 @RequestMapping("game")
 public class GameController {
-    private final HashMap<Integer, Integer> game = new HashMap<>();
-    private final HashMap<Integer, Integer> attempts = new HashMap<>();
+
+    private final GuessingNumberGame game;
 
     @GetMapping
     public String startGame() {
-        int gameId = game.size();
-        game.put(gameId, Math.round((float) Math.random() * 100)
-        );
-        attempts.put(gameId, 0);
-        return "Game started with id: " + gameId;
+        return game.startGame();
     }
 
     @GetMapping("{id}/guess/{number}")
     public String guess(@PathVariable int id, @PathVariable int number) {
-        if (!game.containsKey(id)) {
-            return "Game not found";
-        }
-        attempts.put(id, attempts.get(id) + 1);
-        if (game.get(id) == number) {
-            return "Congratulations! You guessed the number in " + attempts.get(id) + " attempts";
-        } else if (game.get(id) < number) {
-            return "Try smaller number";
-        } else {
-            return "Try bigger number";
-        }
+        return game.guess(id, number);
     }
 }
---
+++ src/main/java/com/example/springguessingnumbergame/services/GuessingNumberGame.java
@@ -0,0 +1,32 @@
+package com.example.springguessingnumbergame.services;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+@Service
+public class GuessingNumberGame {
+    private final HashMap<Integer, Integer> game = new HashMap<>();
+    private final HashMap<Integer, Integer> attempts = new HashMap<>();
+
+    public String startGame() {
+        int gameId = game.size();
+        game.put(gameId, Math.round((float) Math.random() * 100));
+        attempts.put(gameId, 0);
+        return "Game started with id: " + gameId;
+    }
+
+    public String guess(int id, int number) {
+        if (!game.containsKey(id)) {
+            return "Game not found";
+        }
+        attempts.put(id, attempts.get(id) + 1);
+        if (game.get(id) == number) {
+            return "Congratulations! You guessed the number in " + attempts.get(id) + " attempts";
+        } else if (game.get(id) < number) {
+            return "Try smaller number";
+        } else {
+            return "Try bigger number";
+        }
+    }
+}
